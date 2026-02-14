aaeb704edd23be723d98e619e82ed1adb6ba6433
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-14 21:06:10 +0530

    feat: classroom create implementation

---
+++ src/pages/classes/create.tsx
@@ -0,0 +1,212 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { CreateView } from "@/components/refine-ui/views/create-view";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useBack } from "@refinedev/core";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+import { classSchema } from "@/lib/schema";
+
+import * as z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const teachers = [
+  { id: "1", name: "John Doe" },
+  { id: "2", name: "Jane Smith" },
+  { id: "3", name: "Dr. Alan Turing" },
+];
+
+export const subjects = [
+  { id: 1, name: "Mathematics", code: "MATH" },
+  { id: 2, name: "Computer Science", code: "CS" },
+  { id: 3, name: "Physics", code: "PHY" },
+  { id: 4, name: "Chemistry", code: "CHEM" },
+];
+
+const Create = () => {
+  const back = useBack();
+
+  const form = useForm({
+    resolver: zodResolver(classSchema),
+    refineCoreProps: {
+      resource: "classes",
+      action: "create",
+    },
+    defaultValues: {
+      status: "active",
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting, control },
+  } = form;
+
+  const onSubmit = (values: z.infer<typeof classSchema>) => {
+    try {
+      console.log(values);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <CreateView className="class-view">
+      <Breadcrumb />
+
+      <h1 className="page-title">Create a Class</h1>
+
+      <div className="intro-row">
+        <p>Provide the required information below to add a class.</p>
+        <Button onClick={back}>Go Back</Button>
+      </div>
+
+      <Separator />
+
+      <div className="my-4 flex items-center">
+        <Card className="class-form-card">
+          <CardHeader className="relative z-10">
+            <CardTitle className="text-2xl pb-0 font-bold">
+              Fill out the form
+            </CardTitle>
+          </CardHeader>
+
+          <Separator />
+
+          <CardContent className="mt-7">
+            <Form {...form}>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <div className="space-y-3">
+                  <Label>
+                    Banner Image
+                    <span className="text-orange-600">*</span>
+                  </Label>
+
+                  <p>Upload image widget</p>
+                </div>
+                <FormField
+                  control={control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Class Name <span className="text-orange-600">*</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Introduction to Biology - Section A"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="grid sm:grid-cols-2 gap-4">
+                  <FormField
+                    control={control}
+                    name="subjectId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Subject <span className="text-orange-600">*</span>
+                        </FormLabel>
+
+                        <Select
+                          onValueChange={(value) =>
+                            field.onChange(Number(value))
+                          }
+                          value={field?.value?.toString()}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="Select a subject" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {subjects.map((subject) => (
+                              <SelectItem
+                                value={subject.id.toString()}
+                                key={subject.id}
+                              >
+                                {subject.name} ({subject.code})
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={control}
+                    name="subjectId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Subject <span className="text-orange-600">*</span>
+                        </FormLabel>
+
+                        <Select
+                          onValueChange={(value) =>
+                            field.onChange(Number(value))
+                          }
+                          value={field?.value?.toString()}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="Select a subject" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {subjects.map((subject) => (
+                              <SelectItem
+                                value={subject.id.toString()}
+                                key={subject.id}
+                              >
+                                {subject.name} ({subject.code})
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </CreateView>
+  );
+};
+
+export default Create;
