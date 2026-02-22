7ebbb6dda35e2957afd5aaa2519d323f142908d0
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-22 15:51:04 +0530

    feat: added toast error message using arcjet security

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,11 +1,29 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
 if (!BACKEND_BASE_URL)
   throw new Error(
     "BACKEND_BASE_URL is not configured. Please set VITE_BACKEND_BASE_URL in your application"
   );
+
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+  let message = "Request failed";
+
+  try {
+    const payload = (await response.json()) as { message?: string };
+
+    if (payload?.message) message = payload.message;
+  } catch (error) {
+    //Ignore errrors
+  }
+
+  return {
+    message,
+    statusCode: response.status,
+  };
+};
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -32,12 +50,16 @@ const options: CreateDataProviderOptions = {
     },
 
     mapResponse: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
